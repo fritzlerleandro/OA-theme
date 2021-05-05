e8a143fac37e520aabcf227f78a4aaaffe3056b9
Leandro Andrés Fritzler <hola@leandrofritzler.com.ar>
2021-05-05 16:50:13 -0300

    Add files via upload

---
+++ woocommerce/woocommerce.php
@@ -0,0 +1,21 @@
+<?php get_header();?>
+
+
+<div class="content">
+
+    <div class="container pt-3 mb-5">
+
+        <div class="row">
+            
+            <div class="col-12">
+
+                <?php woocomerce_content(); ?>
+
+            </div>
+
+        </div>
+    </div>
+    
+</div>
+
+<?php get_footer();?>
