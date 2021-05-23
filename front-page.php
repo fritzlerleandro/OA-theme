069b49389b565e4d30376a4f3e37b30f8584aeac
Leandro Andrés Fritzler <hola@leandrofritzler.com.ar>
2021-05-23 15:48:21 -0300

    Update front-page.php

--- front-page.php
+++ front-page.php
@@ -10,8 +10,8 @@
 
     <div class="d-flex col-sm-12 justify-content-center">
       <a class="home-access align-self-center" href="<?php bloginfo('url');?>/portfolio/todos">
-        <div class="p-1 titulo-principal">OFICINA AMBULANTE</div>
-        <div class="h5 subtitulo-principal">ESCRITORIO DE ARQUITECTURA</div>
+        <div class="p-1 titulo-principal" style="text-align:center">OFICINA AMBULANTE</div>
+        <div class="h5 subtitulo-principal" style="text-align:center">ESCRITORIO DE ARQUITECTURA</div>
       </a>
     </div>
 
