72c906b66c403a167a91a70fcaa8a381f6de9b52
Leandro Andrés Fritzler <hola@leandrofritzler.com.ar>
2021-05-23 14:59:22 -0300

    cambios en front-page slider

--- front-page.php
+++ front-page.php
@@ -1,26 +1,18 @@
 <?php get_header('no-menu');?>
-<div>
 
-    <div class="parent d-flex">
-
-    <div id="cf" class="d-flex">
-        <img class="img-fluid" src="<?php the_field('imagen_1') ?>" />
-        <img class="img-fluid" src="<?php the_field('imagen_2') ?>" />
-        <img class="img-fluid" src="<?php the_field('imagen_3') ?>" />
-        <img class="img-fluid" src="<?php the_field('imagen_4') ?>" />
-        <img class="img-fluid" src="<?php the_field('imagen_5') ?>" />
-        
+<div class="crossfade">
+      <figure></figure>
+      <figure></figure>
+      <figure></figure>
+      <figure></figure>
+      <figure></figure>
     </div>
-    
+
     <div class="d-flex col-sm-12 justify-content-center">
-        <a class="home-access align-self-center" href="<?php bloginfo('url');?>/portfolio/todos">
-            <div class="p-1 titulo-principal"> OFICINA AMBULANTE</div>
-            <div class="h5 subtitulo-principal"> ESCRITORIO DE ARQUITECTURA </div>
-        </a>
+      <a class="home-access align-self-center" href="#">
+        <div class="p-1 titulo-principal">OFICINA AMBULANTE</div>
+        <div class="h5 subtitulo-principal">ESCRITORIO DE ARQUITECTURA</div>
+      </a>
     </div>
 
-
-    </div>
-
-</div>
 <?php get_footer('no-links');?>
--- header-no-menu.php
+++ header-no-menu.php
@@ -4,6 +4,85 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Oficina Ambulante - Arquitectura & Mobiliario</title>
+    <style>
+        html {
+  margin: 0;
+  padding: 0;
+  height: 100%;
+}
+
+body {
+  margin: 0;
+  padding: 0;
+  height: 100%;
+  max-height: 100%;
+  float: left;
+  width: 100%;
+}
+figure {
+  display: block;
+  margin-block-start: 0;
+  margin-block-end: 0;
+  margin-inline-start: 0;
+  margin-inline-end: 0;
+}
+
+.crossfade > figure {
+  animation: imageAnimation 30s linear infinite 0s;
+  backface-visibility: hidden;
+  background-size: cover;
+  background-position: center center;
+  color: transparent;
+  height: 100%;
+  left: 0px;
+  opacity: 0;
+  position: absolute;
+  top: 0px;
+  width: 100%;
+  z-index: 0;
+}
+
+.crossfade > figure:nth-child(1) {
+  background-image: url("<?php the_field('imagen_1') ?>");
+}
+.crossfade > figure:nth-child(2) {
+  animation-delay: 6s;
+  background-image: url("<?php the_field('imagen_2') ?>");
+}
+.crossfade > figure:nth-child(3) {
+  animation-delay: 12s;
+  background-image: url("<?php the_field('imagen_3') ?>");
+}
+.crossfade > figure:nth-child(4) {
+  animation-delay: 18s;
+  background-image: url("<?php the_field('imagen_4') ?>");
+}
+.crossfade > figure:nth-child(5) {
+  animation-delay: 24s;
+  background-image: url("<?php the_field('imagen_5') ?>");
+}
+
+@keyframes imageAnimation {
+  0% {
+    animation-timing-function: ease-in;
+    opacity: 0;
+  }
+  8% {
+    animation-timing-function: ease-out;
+    opacity: 1;
+  }
+  17% {
+    opacity: 1;
+  }
+  25% {
+    opacity: 0;
+  }
+  100% {
+    opacity: 0;
+  }
+}
+
+    </style>
     <?php wp_head();?>
 </head>
 
