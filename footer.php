b9a2bae35e1967593f7b5e1f4ec04f57a96737f6
Leandro Andrés Fritzler <hola@leandrofritzler.com.ar>
2021-08-19 18:12:33 -0300

    feat: add customizable footer via widgets

--- footer.php
+++ footer.php
@@ -1,12 +1,41 @@
 <footer class="d-flex container main-footer mt-5">
+    <div class="pure-g">
+        
+        <div class="pure-u-1 pure-u-sm-1 pure-u-md-1 pure-u-lg-1-5 pure-u-xl-1-5">
+            
+            <?php dynamic_sidebar( 'footer_area_one' ); ?>
+            
+        </div>
+
+        <div class="pure-u-1 pure-u-sm-1-3 pure-u-md-1-3 pure-u-lg-1-5 pure-u-xl-1-5">
+            
+            <?php dynamic_sidebar( 'footer_area_two' ); ?>
+            
+        </div>
+        
+        <div class="pure-u-1 pure-u-sm-1-3 pure-u-md-1-3 pure-u-lg-1-5 pure-u-xl-1-5">
+            
+            <?php dynamic_sidebar( 'footer_area_three' ); ?>
+            
+        </div>
+        
+        <div class="pure-u-1 pure-u-sm-1-3 pure-u-md-1-3 pure-u-lg-1-5 pure-u-xl-1-5">
+            
+            <?php dynamic_sidebar( 'footer_area_four' ); ?>
+            
+        </div>
+        
+    </div>
+    <div class="chivo pt-3 col-sm-12 col-md-12 col-ls-12 d-flex justify-content-center"><p>Desarrollado por <a href="https://www.leandrofritzler.com.ar">Leandro Andrés Fritzler</a></p></div>
+<!--     
     <div class="d-flex full footer-content border-top border-dark">
         <div class="contact-infos col-sm-12 col-md-6 mt-3">
-            <p class="OA-address"><a class="direccion" href="#"><br>Parana, Entre Rios</a></p>
+            <p class="OA-address"><a class="direccion" href="#"><br>Parana, Entre Rios</a></p> -->
             <!-- <br> -->
-            <p class="OA-email"><a class="footer-email" href="mailto:contacto@oficinaambulante.com.ar">contacto@oficinaambulante.com.ar</a></p>
+            <!-- <p class="OA-email"><a class="footer-email" href="mailto:contacto@oficinaambulante.com.ar">contacto@oficinaambulante.com.ar</a></p> -->
             <!-- <p class="OA-email"><a class="email" href="tel:+5493434611978">(343) 461 1978</a></p>
             <p class="OA-email"><a class="email" href="tel:+5493436115114">(343) 611 5114</a></p> -->
-        </div>
+        <!-- </div>
         <div class="social-links col-sm-12 col-md-6 mt-3">
             <ul>
                 <li><a class="redes" href="https://www.facebook.com/oficinaambulante/" target="_blank">facebook</a></li>
@@ -14,12 +43,8 @@
                 <li><a class="redes" href="https://www.instagram.com/oficinaambulante.mobiliario" target="_blank">instagram OA Mobiliario</a></li>
             </ul>
         </div>
-        <div class="chivo pt-3 col-sm-12 col-md-12 col-ls-12 d-flex justify-content-center"><p>Desarrollado por <a href="https://www.leandrofritzler.com.ar">Leandro Andrés Fritzler</a></p></div>
-    </div>
+    </div> -->
 </footer>
-
-
-
  <?php wp_footer();?>
 </body>
 </html>
