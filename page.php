c4d23864c5b4a551dc19ca43fc7e5166518afc8c
Leandro Andrés Fritzler <hola@leandrofritzler.com.ar>
2021-08-18 21:05:39 -0300

    feat: add class to h1 in pages to edit with fonts

--- page.php
+++ page.php
@@ -5,7 +5,7 @@
 
     <div class="container">
 
-         <h1><?php the_title()?></h1>
+         <h1 class="page_h1"><?php the_title()?></h1>
 
         <?php if(have_posts()) : while(have_posts()) : the_post();?>
 
