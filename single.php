3c3dc91db09b12022a004f7bd58210add85c80a6
Leandro Andrés Fritzler <hola@leandrofritzler.com.ar>
2021-08-22 11:13:29 -0300

    fix: checks if m2 is filled first, then shows it

--- single.php
+++ single.php
@@ -12,9 +12,11 @@
         </div>
         <h1 class="proyecto"><?php the_title()?></h1>
          <p class="mb-1 text-center subtitulos"><?php the_field('lugar') ?></p>
-         <p class="mb-1 text-center subtitulos"><?php the_field('metros_cuadrados')?></p>
+         <?php if( get_field('metros_cuadrados') ): ?>
+         <p class="mb-1 text-center subtitulos">
+            <?php the_field('metros_cuadrados'); echo 'm<sup>2</sup>'; ?> </p>
+         <?php endif; ?>
          <p class="mb-1 text-center subtitulos"><?php the_field('descripcion') ?></p>
-
          <?php $post_slug = get_post_field( 'post_name', get_post() ); ?>
 
         <?php if(have_posts()) : while(have_posts()) : the_post();?>
